474143b3ba5d71db9c2a24aa2bafa67e8f62bc44
shajidur <shajidurrahmansaad10@gamil.com>
2021-02-17 12:33:52 +0600

    modifyed

--- Main.java
+++ Main.java
@@ -12,12 +12,13 @@ public class Main {
         int number = scanner.nextInt();
 
         //second part
-        if (number % 5 == 0)
+        if (number % 3 == 0 && number % 5 == 0)
+            System.out.println("FizzBuzz");
+        else if (number % 5 == 0)
             System.out.println("Fizz");
         else if (number % 3 == 0)
             System.out.println("Buzz");
-        else if (number % 3 == 0 && number % 5 == 0)
-            System.out.println("FizzBuzz");
+
         else
             System.out.println(number);
 
