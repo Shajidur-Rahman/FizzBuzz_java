1b711808aa603fe2a2483f3f072c1cfd7dd8aaf0
shajidur <shajidurrahmansaad10@gamil.com>
2021-02-17 12:26:43 +0600

    file added

---
+++ Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //first part
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+
+        //second part
+        if (number % 5 == 0)
+            System.out.println("Fizz");
+        else if (number % 3 == 0)
+            System.out.println("Buzz");
+        else if (number % 3 == 0 && number % 5 == 0)
+            System.out.println("FizzBuzz");
+        else
+            System.out.println(number);
+
+    }
+}
+
